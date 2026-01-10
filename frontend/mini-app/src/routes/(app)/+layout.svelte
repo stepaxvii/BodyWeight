2fb708a2f1550645dc475b957d4c7b02430bbc75
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2026-01-10 17:19:07 +0300

    refactoring and demo pages

--- frontend/mini-app/src/routes/(app)/+layout.svelte
+++ frontend/mini-app/src/routes/(app)/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import '../app.css';
+	import '../../app.css';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import { PixelNav } from '$lib/components/ui';
